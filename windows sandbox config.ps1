ceb2bb26b220ddf3c5ab2636c0addda6b2673b42
elnao <sgrb7yfe@gmail.com>
2024-09-19 08:49:02 -0500

    Added Offline Mode
    Still more work to be done

--- windows sandbox config.ps1
+++ windows sandbox config.ps1
@@ -1,22 +1,34 @@
+$internet_access = tnc -InformationLevel Quiet
+
 $ProgressPreference = 'SilentlyContinue'
 cd C:\Users\WDAGUtilityAccount\Desktop
 
-# Download files
-invoke-webrequest https://www.python.org/ftp/python/3.9.7/python-3.9.7-amd64.exe -outfile python_install.exe
-invoke-webrequest https://github.com/DidierStevens/DidierStevensSuite/archive/refs/heads/master.zip -outfile didier_master.zip
-invoke-webrequest https://github.com/danielbohannon/Invoke-Obfuscation/archive/refs/heads/master.zip -OutFile obfuscate_master.zip
-Invoke-WebRequest https://github.com/danielbohannon/Revoke-Obfuscation/archive/refs/heads/master.zip -OutFile revoke_master.zip
-invoke-webrequest https://download.mikestammer.com/bstrings.zip -OutFile bstrings.zip
-invoke-webrequest https://download.mikestammer.com/net6/EZViewer.zip -OutFile EZViewer.zip
-invoke-webrequest https://download.sysinternals.com/files/Sysmon.zip -Outfile Sysmon.zip
-invoke-webrequest https://raw.githubusercontent.com/Neo23x0/sysmon-config/master/sysmonconfig-export.xml -OutFile sysmonconfig-export.xml
+if ($internet_access)
+{
+    # Download files
+    invoke-webrequest https://www.python.org/ftp/python/3.9.7/python-3.9.7-amd64.exe -outfile python_install.exe
+    invoke-webrequest https://github.com/DidierStevens/DidierStevensSuite/archive/refs/heads/master.zip -outfile didier_master.zip
+    # invoke-webrequest https://github.com/danielbohannon/Invoke-Obfuscation/archive/refs/heads/master.zip -OutFile obfuscate_master.zip
+    # invoke-WebRequest https://github.com/danielbohannon/Revoke-Obfuscation/archive/refs/heads/master.zip -OutFile revoke_master.zip
+    invoke-webrequest https://download.mikestammer.com/bstrings.zip -OutFile bstrings.zip
+    invoke-webrequest https://download.mikestammer.com/net6/EZViewer.zip -OutFile EZViewer.zip
+    invoke-webrequest https://download.sysinternals.com/files/Sysmon.zip -Outfile Sysmon.zip
+    invoke-webrequest https://raw.githubusercontent.com/Neo23x0/sysmon-config/master/sysmonconfig-export.xml -OutFile sysmonconfig-export.xml
 
-sleep 5
+    sleep 5
+}
+else
+{
+    cd C:\Users\WDAGUtilityAccount\Desktop\WindowsSandbox
+    Copy-Item *.zip,*.exe,*.xml C:\Users\WDAGUtilityAccount\Desktop\
+}
 
 # Unzip files
+cd C:\Users\WDAGUtilityAccount\Desktop
+expand-archive olefile-master.zip .\olefile
 expand-archive .\didier_master.zip .\Dider_tools
-expand-archive .\obfuscate_master.zip .\Obfuscate_tool
-expand-archive .\revoke_master.zip .\Revoke-Obfuscation_tool
+# expand-archive .\obfuscate_master.zip .\Obfuscate_tool
+# expand-archive .\revoke_master.zip .\Revoke-Obfuscation_tool
 expand-archive .\bstrings.zip .\Zimmerman_tools
 expand-archive .\EZViewer.zip .\Zimmerman_tools
 expand-archive .\Sysmon.zip .\Sysmon
@@ -30,13 +42,24 @@ sleep 60
 $Env:Path += ";c:\program files\python39;c:\program files\python39\scripts"
 py --version
 
-# Upgrade PIP
-py -m pip install --upgrade pip
+if ($internet_access)
+{
+    # Upgrade PIP
+    py -m pip install --upgrade pip
 
-# Install Olefile
-py -m pip install olefile
+    # Install Olefile
+    py -m pip install olefile
 
-sleep 30
+    sleep 30
+}
+else
+{
+    # Install Olefile
+    cd C:\Users\WDAGUtilityAccount\Desktop\olefile\olefile-master
+    python setup.py install
+}
+
+cd C:\Users\WDAGUtilityAccount\Desktop
 
 # Install Sysmon
 move-item -path .\sysmonconfig-export.xml -destination .\sysmon
@@ -45,12 +68,14 @@ move-item -path .\sysmonconfig-export.xml -destination .\sysmon
 # Clean up unneeded files.
 remove-item .\python_install.exe
 remove-item .\didier_master.zip
-remove-item .\obfuscate_master.zip
-remove-item .\revoke_master.zip
+# remove-item .\obfuscate_master.zip
+# remove-item .\revoke_master.zip
 remove-item .\bstrings.zip
 remove-item .\EZViewer.zip
 remove-item .\Sysmon.zip
 remove-item .\sysmon -recurse -force
+remove-item .\olefile-master.zip
+remove-item .\olefile -recurse -force
 
 cd C:\Users\WDAGUtilityAccount\Desktop\Dider_tools\DidierStevensSuite-master
 write-host ""
@@ -87,14 +112,26 @@ write-host -foregroundcolor yellow ".\bstrings.exe --lr reg_path -f C:\Users\WDA
 write-host ""
 start-process powershell
 
-# Install Thunderbird
+if ($internet_access)
+{
+    # Download Thunderbird
+    cd C:\Users\WDAGUtilityAccount\Desktop
+    invoke-webrequest "https://download.mozilla.org/?product=thunderbird-128.1.1esr-SSL&os=win64&lang=en-US" -outfile thunderbird.exe
+    
+    # Download .Net 6
+    Invoke-webrequest "https://download.visualstudio.microsoft.com/download/pr/61881ecd-a425-4053-a420-7f76586d2f60/6daf1af8c05df332eb1c53261fafc07f/dotnet-sdk-6.0.425-win-x64.exe" -outfile dotnet6.exe
+    
+}
+
 cd C:\Users\WDAGUtilityAccount\Desktop
-invoke-webrequest "https://download.mozilla.org/?product=thunderbird-128.1.1esr-SSL&os=win64&lang=en-US" -outfile thunderbird.exe
+
+# Install Thunderbird
 .\thunderbird.exe /S
 
-# Install .Net 6
-Invoke-webrequest "https://download.visualstudio.microsoft.com/download/pr/61881ecd-a425-4053-a420-7f76586d2f60/6daf1af8c05df332eb1c53261fafc07f/dotnet-sdk-6.0.425-win-x64.exe" -outfile dotnet6.exe
+# Install Dot Net 6
 .\dotnet6.exe /install /quiet /norestart
+
+
 
 # Remove Thunderbird and dot net 6 install files
 sleep 120
